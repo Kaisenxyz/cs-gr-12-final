34cb4639083509bb9476c2be49d25fb7ee7ff5d1
Sharma <335700043@EDU.YRDSB.NET>
2023-05-31 10:37:27 -0400

    Update calendar.java

--- calendar.java
+++ calendar.java
@@ -1,3 +1,39 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class calendar {
-    
+    public static void main(String[] args) {
+        while (true) {
+            // Get the current date and time
+            LocalDateTime currentDateTime = LocalDateTime.now();
+
+            // Retrieve individual components of the date and time
+            int year = currentDateTime.getYear();
+            int month = currentDateTime.getMonthValue();
+            int day = currentDateTime.getDayOfMonth();
+
+            // Format the minute to display in 00 style
+
+
+            // Format the hour to display in 12-hour format with AM/PM
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a");
+            String timeFormatted = currentDateTime.format(formatter);
+
+            // Display the current date and time
+            System.out.println("Date: " + month + "/" + day + "/" + year);
+            System.out.println("Time: " + timeFormatted);
+
+            try {
+                // Delay for one second
+                Thread.sleep(1000);
+
+                // Clear the console
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        
+    }
 }
