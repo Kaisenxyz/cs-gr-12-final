a01b10be6914fdbe401f514181708323a469159b
Sharma <335700043@EDU.YRDSB.NET>
2023-06-04 22:13:07 -0400

    arman is him

--- main.java
+++ main.java
@@ -1,3 +1,11 @@
 public class main {
-    
+    public static void main(String[] args) {
+        Thread calendarThread = new Thread(() -> {
+            calendar.displayDateTime();
+        });
+
+        Reminder.main(args);
+
+        calendarThread.start();
+    }
 }
