81db24e4970c0547a7840a17c79cdc2fbf9c745e
HackMinion <132582766+HackMinion@users.noreply.github.com>
2023-06-05 23:25:38 -0400

    Update reminder.java

--- reminder.java
+++ reminder.java
@@ -1,15 +1,17 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Reminder {
     private String taskName;
-    private String dueDate;
+    private LocalDateTime dueDate;
 
-    public Reminder(String taskName, String dueDate) {
+    public Reminder(String taskName, LocalDateTime dueDate) {
         this.taskName = taskName;
         this.dueDate = dueDate;
     }
 
-    public static Reminder createReminder(String taskName, String dueDate) {
+    public static Reminder createReminder(String taskName, LocalDateTime dueDate) {
         return new Reminder(taskName, dueDate);
     }
 
@@ -27,16 +29,40 @@ public class Reminder {
             }
 
             System.out.print("Enter due date (format: YYYY/MM/DD): ");
-            String dueDate = scanner.nextLine();
+            String dueDateInput = scanner.nextLine();
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+            LocalDateTime dueDate = LocalDateTime.parse(dueDateInput, formatter);
 
             Reminder reminder = Reminder.createReminder(taskName, dueDate);
-            reminder.displayReminder();
+            displayReminderAndDateTime(reminder);
         }
     }
 
-    public void displayReminder() {
-        System.out.println("Reminder: " + taskName);
-        System.out.println("Due Date: " + dueDate);
-        System.out.println();
+    public static void displayReminderAndDateTime(Reminder reminder) {
+        System.out.println("Reminder: " + reminder.taskName);
+        System.out.println("Due Date: " + reminder.dueDate);
+
+        // Get the current date and time
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        // Format the hour to display in 12-hour format with AM/PM
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a");
+        String timeFormatted = currentDateTime.format(formatter);
+
+        // Display the current date and time
+        System.out.println("Date: " + currentDateTime.toLocalDate());
+        System.out.println("Time: " + timeFormatted);
+
+        try {
+            // Delay for one second
+            Thread.sleep(1000);
+
+            // Clear the console
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
