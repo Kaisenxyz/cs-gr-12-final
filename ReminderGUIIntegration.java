624b41da49cbfdedd9e0d4f42f711d893ccac58c
HackMinion <132582766+HackMinion@users.noreply.github.com>
2023-06-11 21:44:20 -0400

    Update ReminderGUIIntegration.java

--- ReminderGUIIntegration.java
+++ ReminderGUIIntegration.java
@@ -5,13 +5,16 @@ public class ReminderGUIIntegration {
     private static GUIm gui;
 
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(ReminderGUIIntegration::createAndShowGUI);
+        SwingUtilities.invokeLater(ReminderGUIIntegration::createAndShowGUI);
         Reminder.main(args);
     }
 
     private static void createAndShowGUI() {
         gui = new GUIm();
-        gui.createAndShowGUI();
+
+        // Example usage of the addReminder method
+        LocalDateTime dueDate = LocalDateTime.now().plusDays(2);
+        gui.addReminder("Task 1", dueDate);
     }
 
     public static void registerReminder(String taskName, LocalDateTime dueDate) {
